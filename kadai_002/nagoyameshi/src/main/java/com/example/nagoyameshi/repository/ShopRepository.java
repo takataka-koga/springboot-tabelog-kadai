fefd414a6f71324f8acd1f01b3c95b33f5abbfa4
takataka-koga <149985862+takataka-koga@users.noreply.github.com>
2024-02-13 13:45:33 +0900

    Delete kadai_002/nagoyameshi directory

--- kadai_002/nagoyameshi/src/main/java/com/example/nagoyameshi/repository/ShopRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.nagoyameshi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nagoyameshi.entity.Shop;
-public interface ShopRepository extends JpaRepository<Shop, Integer> {
-
-}
